0c277b2c73eb20e4ee971fd1bc836d190fb3725d
mainsw <mainwipe@gmail.com>
2022-11-10 22:45:29 +0900

    setup.py 생성, 구조 변경

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeekdga',
+    version='0.0.1',
+    description='DGA Domain Detector utilizing Zeek DNS logs and Deep Learning',
+    author='mainsw',
+    url='https://github.com/mainsw/Zeek-DGA-Detector.git',
+    packages=['src'],
+    install_requires=[
+        'dgaintel==2.3',
+        'slack-sdk==3.19.2',
+        'zat==0.4.4'
+    ],
+    python_requires='>=3.7',
+    extras_require={'es7': ['elasticsearch==7.0.0'], 'es8': ['elasticsearch==8.0.0']},
+    entry_points={
+        'console_scripts': [
+            'zeekdga = src.zeekdga',
+        ],
+    }
+)
