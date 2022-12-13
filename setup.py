637592a8bdbf4e8a79070301275917f107fc272d
mainsw <mainwipe@gmail.com>
2022-12-13 14:32:22 +0900

    version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeekdga',
-    version='0.0.8',
+    version='1.0.0',
     description='DGA Domain Detector utilizing Zeek DNS logs and Deep Learning',
     author='mainsw',
     url='https://github.com/mainsw/Zeek-DGA-Detector.git',
